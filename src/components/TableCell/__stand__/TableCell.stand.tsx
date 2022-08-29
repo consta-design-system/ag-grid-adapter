a05d64fda15126904f1755f8eb027f6ac3ead4ac
N1MBER <socialfox33@gmail.com>
2022-08-29 11:33:19 +0300

    docs(docs): add figma links and fix links to uikit

--- src/components/TableCell/__stand__/TableCell.stand.tsx
+++ src/components/TableCell/__stand__/TableCell.stand.tsx
@@ -6,5 +6,7 @@ export default createStand({
   group: 'components',
   description: 'Этот компонент кастомизирует ячейку таблицы.',
   version: '0.1.1',
+  figma:
+    'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D404%253A29189',
   status: 'stable',
 });
--- src/components/TableEditorText/__stand__/TableEditorText.stand.tsx
+++ src/components/TableEditorText/__stand__/TableEditorText.stand.tsx
@@ -7,5 +7,7 @@ export default createStand({
   description:
     'Компонент редактора для изменения строковых значений в ячейке таблицы.',
   version: '0.1.1',
+  figma:
+    'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D404%253A29189',
   status: 'stable',
 });
