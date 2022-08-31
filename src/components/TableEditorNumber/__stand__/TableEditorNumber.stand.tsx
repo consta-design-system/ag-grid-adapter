4b96006da0148c492116e2e8d244105d44be7eff
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-08-31 15:04:31 +0300

    docs(documentation): add docs for ag-grid stand (#12)
    * docs(documentation): add docs for ag-grid stand
    
    consta-design-system/uikit#2568
    
    * docs(docs): add figma links and fix links to uikit

--- src/components/TableEditorNumber/__stand__/TableEditorNumber.stand.tsx
+++ src/components/TableEditorNumber/__stand__/TableEditorNumber.stand.tsx
@@ -1,10 +1,11 @@
 import { createStand } from '##/stand/standConfig';
 
 export default createStand({
-  title: 'agGridAdapter',
-  id: 'agGridAdapter',
-  group: 'adapters',
-  description: 'Этот адаптер стилизует таблицу.',
+  title: 'TableEditorNumber',
+  id: 'TableEditorNumber',
+  group: 'components',
+  description:
+    'Компонент редактора для изменения числовых значений в ячейке таблицы.',
   version: '0.1.1',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D404%253A29189',
