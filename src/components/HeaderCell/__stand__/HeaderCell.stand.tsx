ba6350b823c42a55d31b7e5674eac09fc13d7a99
arhayka <arhayka@gmail.com>
2022-09-02 11:46:18 +0300

    docs(aggrid): edit docs (#13)

--- src/components/HeaderCell/__stand__/HeaderCell.stand.tsx
+++ src/components/HeaderCell/__stand__/HeaderCell.stand.tsx
@@ -4,7 +4,7 @@ export default createStand({
   title: 'HeaderCell',
   id: 'HeaderCell',
   group: 'components',
-  description: 'Элемент заголовка таблицы',
+  description: 'Компонент для кастомизации заголовка столбца.',
   version: '0.1.1',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D404%253A29189',
--- src/components/TableEditorText/__stand__/TableEditorText.stand.tsx
+++ src/components/TableEditorText/__stand__/TableEditorText.stand.tsx
@@ -4,8 +4,7 @@ export default createStand({
   title: 'TableEditorText',
   id: 'TableEditorText',
   group: 'components',
-  description:
-    'Компонент редактора для изменения строковых значений в ячейке таблицы.',
+  description: 'Редактор для изменения строк в ячейке таблицы.',
   version: '0.1.1',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D404%253A29189',
