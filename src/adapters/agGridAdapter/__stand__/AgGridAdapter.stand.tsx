b91da149c4dd68bf9c6a49b617b7afaeb3267aba
N1MBER <socialfox33@gmail.com>
2022-09-13 18:14:27 +0300

    docs(sandbox): add sandboxes links
    consta-design-system/uikit#2615

--- src/adapters/agGridAdapter/__stand__/AgGridAdapter.stand.tsx
+++ src/adapters/agGridAdapter/__stand__/AgGridAdapter.stand.tsx
@@ -6,6 +6,8 @@ export default createStand({
   group: 'adapters',
   description: 'Этот адаптер стилизует таблицу.',
   version: '0.1.1',
+  sandbox:
+    'https://codesandbox.io/embed/aggridadapter-4bufgz?fontsize=14&hidenavigation=1&theme=dark',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D404%253A29189',
   status: 'stable',
--- src/components/TableCell/__stand__/TableCell.stand.tsx
+++ src/components/TableCell/__stand__/TableCell.stand.tsx
@@ -6,6 +6,8 @@ export default createStand({
   group: 'components',
   description: 'Этот компонент кастомизирует ячейку таблицы.',
   version: '0.1.1',
+  sandbox:
+    'https://codesandbox.io/embed/tablecell-4qzksv?fontsize=14&hidenavigation=1&theme=dark',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D404%253A29189',
   status: 'stable',
