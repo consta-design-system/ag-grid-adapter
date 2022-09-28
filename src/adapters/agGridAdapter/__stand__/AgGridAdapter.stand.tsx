ee5da42bfb5b7b6266d59ad1accfc54cc3f2ce45
Anya Ovsyannikova <arhayka@gmail.com>
2022-09-28 11:58:41 +0300

    docs: change versions, check imports

--- src/adapters/agGridAdapter/__stand__/AgGridAdapter.stand.tsx
+++ src/adapters/agGridAdapter/__stand__/AgGridAdapter.stand.tsx
@@ -5,7 +5,7 @@ export default createStand({
   id: 'agGridAdapter',
   group: 'adapters',
   description: 'Адаптер для таблицы ag-grid-react.',
-  version: '0.1.1',
+  version: '0.0.1',
   sandbox: 'aggridadapter-4bufgz',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D404%253A29189',
--- src/components/TableEditorText/__stand__/TableEditorText.stand.tsx
+++ src/components/TableEditorText/__stand__/TableEditorText.stand.tsx
@@ -6,7 +6,7 @@ export default createStand({
   group: 'components',
   description:
     'Компонент редактора для изменения строковых значений в ячейке таблицы.',
-  version: '0.1.1',
+  version: '0.1.0',
   sandbox: 'tableeditortext-4h1k2z',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D404%253A29189',
