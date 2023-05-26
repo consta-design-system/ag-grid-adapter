ac86a0b87db4b0e097ebd033337afaa5fa1ca8db
gizeasy <gizeasy@gmail.com>
2023-05-26 11:04:47 +0300

    docs(standConfig): add figmaUrl

--- src/stand/standConfig.ts
+++ src/stand/standConfig.ts
@@ -24,4 +24,6 @@ export const { createStand } = createConfig({
   description:
     'Адптер для библиотеки ag-grid-react. Стилизует таблицу для дизайн-системы Consta.',
   repositoryUrl: 'https://github.com/consta-design-system/ag-grid-adapter',
+  figmaUrl:
+    'https://www.figma.com/file/LKYyjgQ3khFHvXraZAG1n1?embed_host=share&kind=&node-id=404%3A29189&viewer=1',
 });
